4a38667a7bc78bc85fd4b8fd7a7936ba54b8471c
Bouh Ilyas <47904709+L-Yess@users.noreply.github.com>
2019-10-20 20:05:56 +0100

    Update AppDelegate.m

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -8,8 +8,8 @@
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  [GMSServices provideAPIKey:@"AIzaSyB9aNCUDcq0LDmcg4zYA0bcHltX82iQiL8"];
-  [GMSPlacesClient provideAPIKey:@"AIzaSyB9aNCUDcq0LDmcg4zYA0bcHltX82iQiL8"];
+  [GMSServices provideAPIKey:@"YOUR_API_TOKEN"];
+  [GMSPlacesClient provideAPIKey:@"YOUR_API_TOKEN"];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
